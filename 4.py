5c96d349d1055033360514fc5679c4ef0a47bd5f
asankina-wq <a.sankina@g.nsu.ru>
2026-03-13 23:28:25 +0700

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,11 +1,6 @@
 text = input("Введите ваш текст: ")
-count = {}
+
 for letter in text:
-    if letter in count:
-        count[letter] +=1
-    else:
-        count[letter] = 1
-for letter in count:
-    if count[letter] ==3:
-        print("Элемент, встречающийся ровно 3 раза: ", letter)
+    if text.count(letter) == 3:
+        print("Элемент, встречающийся ровно 3 раза:", letter)
         break
