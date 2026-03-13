8c5c743ee4dd1b97974f3e50de7a4bc0c81b2201
asankina-wq <a.sankina@g.nsu.ru>
2026-03-13 23:17:39 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,7 @@
 text = input("Введите ваш текст: ")
 max_spaces = 0
 current_spaces = 0
+
 for symbol in text:
     if symbol.isspace():
         current_spaces += 1
@@ -8,4 +9,4 @@ for symbol in text:
             max_spaces = current_spaces
     else:
         current_spaces = 0
-print(" Максимальное количество: ", max_spaces)
+print("Максимальное количество: ", max_spaces)
