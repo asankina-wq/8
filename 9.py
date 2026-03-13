4face77d92c2235f4a25cd36ae641c84ce7b5566
asankina-wq <a.sankina@g.nsu.ru>
2026-03-13 23:37:01 +0700

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,11 +1,7 @@
 text = input("Введите ваш текст: ")
 words = text.split()
-amount = {}
+
 for word in words:
-    if word in amount:
-        amount[word] += 1
-    else:
-        amount[word] = 1
-for word in amount:
-    if amount[word] ==2:
-        print("Слово, повторяющееся дважды: ", word)
+    if words.count(word) == 2:
+        print("Слово, повторяющееся дважды:", word)
+        break
