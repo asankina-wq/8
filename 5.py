c494d734efb358464786fab6579809a80680330e
asankina-wq <a.sankina@g.nsu.ru>
2026-03-13 23:30:37 +0700

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,9 +1,12 @@
 text_1 = input("Введите текст: ")
 text_2 = input("Введите текст: ")
 text_3 = input("Введите текст: ")
+
 set1 = set(text_1)
 set2 = set(text_2)
 set3 = set(text_3)
+
 result = (set1 - set2 - set3) | (set2 - set1 - set3) | (set3 - set1 -set2)
+
 for letter in result:
     print("Неповторяющийся элемент: ", letter)
