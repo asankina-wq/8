d396b1058e9cdf267c99db15b5498f28734a661f
asankina-wq <a.sankina@g.nsu.ru>
2026-03-13 23:26:53 +0700

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,6 +1,8 @@
 text = input("Введите ваш текст: ")
 letters = set()
+
 for letter in text:
     if letter.isalpha():
         letters.add(letter.lower())
+        
 print("Количество разных букв: ", len(letters))
